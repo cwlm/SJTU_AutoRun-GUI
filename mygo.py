28831c893cd3433096039faaf58157324395b941
cwlm_ma <cwlm_ma@sjtu.edu.cn>
2024-08-27 16:29:56 +0800

    fix: delete useless info

--- mygo.py
+++ mygo.py
@@ -9,14 +9,8 @@ st.set_page_config(
 )
 
 
-# ==================== 边栏 ====================
-with st.sidebar:
-    st.success("选择一项功能")
-
-
 # ==================== 正文 ====================
 st.write("# 欢迎来到交我润控制台!")
-st.write("👈 请在左侧选择功能")
 with open("Hello.md", "r", encoding="utf-8") as f:
     readme = f.read()
 st.markdown(readme)
